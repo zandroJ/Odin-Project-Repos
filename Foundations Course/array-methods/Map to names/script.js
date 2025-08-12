2665e7d9733122d31db16d59826be7ea2f04cd4d
zandroJ <70626244+zandroJ@users.noreply.github.com>
2025-08-12 15:26:15 +0400

    Delete Foundations Course directory

--- Foundations Course/array-methods/Map to names/script.js
+++
@@ -1,9 +0,0 @@
-let john = { name: "John", age: 25 };
-let pete = { name: "Pete", age: 30 };
-let mary = { name: "Mary", age: 28 };
-
-let users = [ john, pete, mary ];
-
-let names = users.map(item => item.name);
-
-alert( names );
