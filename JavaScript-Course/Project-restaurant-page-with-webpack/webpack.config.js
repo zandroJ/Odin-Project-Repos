6c78604c1f91faed10907fa1a12075d02d568c43
‎ <louisethekidrs@gmail.com>
2026-09-04 15:11:32 +0400

    Update webpack.config.js

--- JavaScript-Course/Project-restaurant-page-with-webpack/webpack.config.js
+++ JavaScript-Course/Project-restaurant-page-with-webpack/webpack.config.js
@@ -4,11 +4,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
+ output: {
+  filename: 'main.js',
+  path: path.resolve(__dirname),
+},
+
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
@@ -23,6 +23,6 @@ module.exports = {
     ],
   },
   devServer: {
-    static: './dist',
+    static: './',
   },
 };
