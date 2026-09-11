56c66542f9d991f8a7e90b2692549a44a89bbf44
‎ <louisethekidrs@gmail.com>
2026-09-11 20:12:47 +0400

    create binary search tree project

---
+++ JavaScript-Course/Project-BinarySearchTree/bst.js
@@ -0,0 +1,276 @@
+class Node{
+constructor(data = null){
+    this.leftChild = null;
+    this.rightChild = null;
+    this.storedData = data;
+}
+}
+
+class Tree {
+    constructor(array) {
+        // Automatically build the tree when a new Tree instance is created
+        this.root = this.buildTree(array, 0, array.length - 1);
+    }
+
+ buildTree(array,start, end){
+if (start > end){
+    return null;
+}
+const mid = Math.floor((start + end) / 2);
+const node = new Node(array[mid]);
+
+node.leftChild = this.buildTree(array, start, mid - 1);
+node.rightChild = this.buildTree(array, mid + 1, end);
+return node;
+}
+includes(value) {
+    let current = this.root; // Start looking from the very top of the tree
+
+    // Loop through the tree until we run out of nodes
+    while (current !== null) {
+        if (value === current.storedData) {
+            return true; // Found it!
+        }
+        
+        // If the value is smaller, go down the left branch
+        if (value < current.storedData) {
+            current = current.leftChild;
+        } 
+        // If the value is bigger, go down the right branch
+        else {
+            current = current.rightChild;
+        }
+    }
+
+    return false; // Walked through the whole tree and didn't find it
+}
+
+insert(value) {
+    const newNode = new Node(value);
+
+    // Case 1: If the tree is completely empty, make this node the root
+    if (this.root === null) {
+        this.root = newNode;
+        return;
+    }
+
+    let current = this.root;
+    let parent = null;
+
+    while (current !== null) {
+        parent = current; // Keep track of the last valid node we visited
+
+        // Requirement: If the value already exists, do nothing
+        if (value === current.storedData) {
+            return; 
+        }
+
+        // If the new value is smaller, head left
+        if (value < current.storedData) {
+            current = current.leftChild;
+        } 
+        // If the new value is larger, head right
+        else {
+            current = current.rightChild;
+        }
+    }
+
+    // Once we hit null, attach the new node to the parent we saved
+    if (value < parent.storedData) {
+        parent.leftChild = newNode;
+    } else {
+        parent.rightChild = newNode;
+    }
+}
+
+deleteItem(value){
+    let current = this.root;
+    let parent = null;
+
+    //finding the node for deletion.
+while(current !== null){
+    if (value === current.storedData){
+        break;
+    }
+
+parent = current;
+if (value < current.storedData){
+    current = current.leftChild;
+}
+else{
+    current = current.rightChild;
+}
+
+}
+
+//if value not found
+if (current === null){
+    return;
+}
+//no child case
+if (current.leftChild === null && current.rightChild === null){
+
+    //delete root
+    if (parent === null){
+        this.root = null;
+    }
+    //current is left child
+    else if (parent.leftChild === current){
+        parent.leftChild = null;
+    }
+    //current is right child
+    else{
+        parent.rightChild = null;
+    }
+}
+
+//one child case
+else if (current.leftChild === null || current.rightChild === null) {
+const child = current.LeftChild !== null ? current.LeftChild : current.rightChild;
+if (current.leftChild === null || current.rightChild === null){
+    if (parent === null){
+        this.root = child;
+    }
+    else if (parent.leftChild === null){
+        parent.leftChild = child;
+    }
+    else{
+        parent.rightChild = child;
+    }
+}
+}
+//two child case
+else{
+let successorParent = current;
+let successor = current.rightChild;
+while (successor.leftChild !== null) {
+    successorParent = successor;
+    successor = successor.leftChild;
+}
+        current.storedData = successor.storedData;
+        if (successorParent.leftChild === successor) {
+            successorParent.leftChild = successor.rightChild;
+        } else {
+            successorParent.rightChild = successor.rightChild;
+        }
+}
+
+
+}
+
+levelOrderForEach(callback) {
+    // Make sure a callback function was provided
+    if (typeof callback !== "function") {
+        throw new Error("A callback function is required");
+    }
+
+    // Use a queue for breadth-first traversal
+    let queue = [this.root];
+
+    while (queue.length > 0) {
+        // Take the first node out of the queue
+        let current = queue.shift();
+
+        // Callback gets the value, not the node
+        callback(current.storedData);
+
+        // Add children to the back of the queue
+        if (current.leftChild !== null) {
+            queue.push(current.leftChild);
+        }
+
+        if (current.rightChild !== null) {
+            queue.push(current.rightChild);
+        }
+    }
+}
+
+inOrderForEach(callback){
+    if (typeof callback !== "function"){
+  throw new Error("A callback function is required");
+    }
+
+    //recursion for traversing
+    const traverse = (node) =>{
+        if(node === null){
+            return;
+        }
+        traverse(node.leftChild);
+        callback(node.storedData);
+        traverse(node.rightChild);
+    };
+traverse(this.root);
+}
+
+preOrderForEach(callback){
+    if (typeof callback !== "function"){
+  throw new Error("A callback function is required");
+    }
+
+    //recursion for traversing
+    const traverse = (node) =>{
+        if(node === null){
+            return;
+        }
+        traverse(node.leftChild);
+        callback(node.storedData);
+        traverse(node.rightChild);
+    };
+traverse(this.root);
+}
+
+postOrderForEach(callback){
+    if (typeof callback !== "function"){
+  throw new Error("A callback function is required");
+    }
+
+    //recursion for traversing
+    const traverse = (node) =>{
+        if(node === null){
+            return;
+        }
+        traverse(node.leftChild);
+        callback(node.storedData);
+        traverse(node.rightChild);
+    };
+traverse(this.root);
+}
+
+height(value) {
+    // Find the node containing the value
+    let current = this.root;
+
+    while (current !== null) {
+        if (value === current.storedData) {
+            break;
+        }
+
+        if (value < current.storedData) {
+            current = current.leftChild;
+        } else {
+            current = current.rightChild;
+        }
+    }
+
+    // Value wasn't found
+    if (current === null) {
+        return undefined;
+    }
+
+    // Recursively find the longest path to a leaf
+    const getHeight = (node) => {
+        if (node === null) {
+            return -1;
+        }
+
+        const leftHeight = getHeight(node.leftChild);
+        const rightHeight = getHeight(node.rightChild);
+
+        return 1 + Math.max(leftHeight, rightHeight);
+    };
+
+    return getHeight(current);
+}
+
+
+}
--- JavaScript-Course/Project-HashMaps/hashmap.js
+++ JavaScript-Course/Project-HashMaps/hashmap.js
@@ -1,168 +1,252 @@
-class HashMap  {
-    constructor(size = 16){
+class HashMap {
+    constructor(size = 16) {
+        // The main array that holds all of our buckets.
+        // Initially, all positions are empty (undefined).
         this.bucket = new Array(size);
-    this.size = 0;
-    this.capacity = size;
-}
-    #hash(key){
+
+        // Tracks how many key/value pairs are actually stored.
+        this.size = 0;
+
+        // Tracks how many buckets we currently have.
+        this.capacity = size;
+    }
+
+    #hash(key) {
+        // Turns a string key into a number (hash code).
         let hashCode = 0;
+
+        // Prime numbers help distribute keys more evenly.
         const primeNumber = 31;
-        for (let i = 0; i < key.length; i++){
-            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+
+        // Go through every character in the key.
+        for (let i = 0; i < key.length; i++) {
+            hashCode =
+                primeNumber * hashCode + key.charCodeAt(i);
         }
+
+        // Convert the hash code into a valid bucket index.
+        // The result will be between 0 and capacity - 1.
         return hashCode % this.capacity;
-        
     }
 
-    set(key,value){
-       const bucketIndex = this.#hash(key);
-       let bucket = this.bucket[bucketIndex];
+    set(key, value) {
+        // Figure out which bucket this key belongs to.
+        const bucketIndex = this.#hash(key);
+
+        // Get that bucket.
+        let bucket = this.bucket[bucketIndex];
+
+        // If the bucket doesn't exist yet, create it.
         if (!bucket) {
-        this.bucket[bucketIndex] = [];
-        bucket = this.bucket[bucketIndex];
+            this.bucket[bucketIndex] = [];
 
-    }
-       for (let i = 0; i < bucket.length; i++){
-        const [storedKey,storedValue] = bucket[i];
-        if(storedKey === key){
-            bucket[i][1] = value;
-            return;
-        }
-       }
-bucket.push([key,value]);
-this.size++;
-if (this.size / this.capacity > 0.75) {
-        this.#resize();
-    }
-    }
-
-    get(key){
-       const bucketIndex = this.#hash(key);
-    
-    // Step 2: Go directly to that bucket
-    const bucket = this.bucket[bucketIndex];
-    
-    if (!bucket) return null; // Bucket is empty
-
-    // Step 3: Traverse the bucket to find the exact key match
-    for (let i = 0; i < bucket.length; i++) {
-        const [storedKey, storedValue] = bucket[i];
-        if (storedKey === key) {
-            return storedValue; // Found it!
-        }
-    }
-    
-    return null; // Key doesn't exist 
-
+            // Make the local variable point to the new bucket.
+            bucket = this.bucket[bucketIndex];
         }
 
- has(key) {
-    const bucketIndex = this.#hash(key);
-    const bucket = this.bucket[bucketIndex];
+        // Search the bucket for an existing key.
+        // This is necessary because different keys can have
+        // the same hash index (a collision).
+        for (let i = 0; i < bucket.length; i++) {
+            const [storedKey, storedValue] = bucket[i];
 
-    if (!bucket) return false;
+            // If the key already exists, update its value.
+            if (storedKey === key) {
+                bucket[i][1] = value;
 
-    for (let i = 0; i < bucket.length; i++) {
-        const [storedKey] = bucket[i];
+                // Don't increase size because we didn't add
+                // a new key/value pair.
+                return;
+            }
+        }
 
-        if (storedKey === key) {
-            return true;
+        // The key didn't already exist,
+        // so add a new [key, value] pair.
+        bucket.push([key, value]);
+
+        // Increase the number of stored entries.
+        this.size++;
+
+        // Check the load factor.
+        // If more than 75% full, resize the HashMap.
+        if (this.size / this.capacity > 0.75) {
+            this.#resize();
         }
     }
 
-    return false;
-}
+    get(key) {
+        // Find which bucket the key should be in.
+        const bucketIndex = this.#hash(key);
 
-remove(key) {
-    const bucketIndex = this.#hash(key);
-    const bucket = this.bucket[bucketIndex];
+        // Get that bucket.
+        const bucket = this.bucket[bucketIndex];
 
-    if (!bucket) return false;
+        // If the bucket doesn't exist, the key isn't present.
+        if (!bucket) return null;
 
-    for (let i = 0; i < bucket.length; i++) {
-        const [storedKey] = bucket[i];
+        // Search through the bucket.
+        for (let i = 0; i < bucket.length; i++) {
+            const [storedKey, storedValue] = bucket[i];
 
-        if (storedKey === key) {
-            bucket.splice(i, 1);
-            this.size--;
-            return true;
+            // If we found the key, return its value.
+            if (storedKey === key) {
+                return storedValue;
+            }
+        }
+
+        // The key wasn't found.
+        return null;
+    }
+
+    has(key) {
+        // Find the correct bucket.
+        const bucketIndex = this.#hash(key);
+        const bucket = this.bucket[bucketIndex];
+
+        // No bucket means the key isn't present.
+        if (!bucket) return false;
+
+        // Search through the bucket.
+        for (let i = 0; i < bucket.length; i++) {
+            const [storedKey] = bucket[i];
+
+            // Found the key!
+            if (storedKey === key) {
+                return true;
+            }
+        }
+
+        // Key wasn't found.
+        return false;
+    }
+
+    remove(key) {
+        // Find the correct bucket.
+        const bucketIndex = this.#hash(key);
+        const bucket = this.bucket[bucketIndex];
+
+        // No bucket means there's nothing to remove.
+        if (!bucket) return false;
+
+        // Search through the bucket.
+        for (let i = 0; i < bucket.length; i++) {
+            const [storedKey] = bucket[i];
+
+            // If we found the key...
+            if (storedKey === key) {
+
+                // Remove 1 item at index i.
+                bucket.splice(i, 1);
+
+                // One less key/value pair exists.
+                this.size--;
+
+                // Tell the caller that removal succeeded.
+                return true;
+            }
+        }
+
+        // Key wasn't found.
+        return false;
+    }
+
+    length() {
+        // Return the number of stored key/value pairs.
+        return this.size;
+    }
+
+    clear() {
+        // Create a completely new empty bucket array.
+        // Keep the current capacity.
+        this.bucket = new Array(this.capacity);
+
+        // There are now zero stored entries.
+        this.size = 0;
+    }
+
+    keys() {
+        // Array that will contain all keys.
+        const keys = [];
+
+        // Go through every bucket.
+        for (let i = 0; i < this.bucket.length; i++) {
+            const bucket = this.bucket[i];
+
+            // Skip empty buckets.
+            if (!bucket) continue;
+
+            // Go through every entry in the bucket.
+            for (let j = 0; j < bucket.length; j++) {
+                // [0] is the key.
+                keys.push(bucket[j][0]);
+            }
+        }
+
+        return keys;
+    }
+
+    values() {
+        // Array that will contain all values.
+        const values = [];
+
+        // Go through every bucket.
+        for (let i = 0; i < this.bucket.length; i++) {
+            const bucket = this.bucket[i];
+
+            // Skip empty buckets.
+            if (!bucket) continue;
+
+            // Go through every entry in the bucket.
+            for (let j = 0; j < bucket.length; j++) {
+                // [1] is the value.
+                values.push(bucket[j][1]);
+            }
+        }
+
+        return values;
+    }
+
+    entries() {
+        // Array that will contain [key, value] pairs.
+        const entries = [];
+
+        // Go through every bucket.
+        for (let i = 0; i < this.bucket.length; i++) {
+            const bucket = this.bucket[i];
+
+            // Skip empty buckets.
+            if (!bucket) continue;
+
+            // Add every [key, value] pair.
+            for (let j = 0; j < bucket.length; j++) {
+                entries.push(bucket[j]);
+            }
+        }
+
+        return entries;
+    }
+
+    #resize() {
+        // Save all existing entries before replacing
+        // the old bucket array.
+        const oldEntries = this.entries();
+
+        // Double the number of buckets.
+        this.capacity *= 2;
+
+        // Create a new, larger empty bucket array.
+        this.bucket = new Array(this.capacity);
+
+        // Reset size because we're about to add
+        // all the entries back into the new map.
+        this.size = 0;
+
+        // Add every old entry back using set().
+        // This is important because the larger capacity
+        // changes the hash indexes, so everything needs
+        // to be rehashed.
+        for (const [key, value] of oldEntries) {
+            this.set(key, value);
         }
     }
-
-    return false;
 }
-
-length() {
-    return this.size;
-}
-clear() {
-    this.bucket = new Array(this.capacity);
-    this.size = 0;
-}
-keys() {
-    const keys = [];
-
-    for (let i = 0; i < this.bucket.length; i++) {
-        const bucket = this.bucket[i];
-
-        if (!bucket) continue;
-
-        for (let j = 0; j < bucket.length; j++) {
-            keys.push(bucket[j][0]);
-        }
-    }
-
-    return keys;
-}
-
-
-    
-
-// values() returns an array containing all the values.
-values() {
-    const values = [];
-
-    for (let i = 0; i < this.bucket.length; i++) {
-        const bucket = this.bucket[i];
-
-        if (!bucket) continue;
-
-        for (let j = 0; j < bucket.length; j++) {
-            values.push(bucket[j][1]);
-        }
-    }
-
-    return values;
-}
-
-
-// entries() returns an array that contains each key, value pair. Example: [[firstKey, firstValue], [secondKey, secondValue]]
-entries() {
-    const entries = [];
-
-    for (let i = 0; i < this.bucket.length; i++) {
-        const bucket = this.bucket[i];
-
-        if (!bucket) continue;
-
-        for (let j = 0; j < bucket.length; j++) {
-            entries.push(bucket[j]);
-        }
-    }
-
-    return entries;
-}
-
-#resize() {
-    const oldEntries = this.entries();
-
-    this.capacity *= 2;
-    this.bucket = new Array(this.capacity);
-    this.size = 0;
-
-    for (const [key, value] of oldEntries) {
-        this.set(key, value);
-    }
-}
-}
-
